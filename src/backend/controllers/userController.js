df17aa979fd312b84a060bb364632708fb2deb80
Anjul <rathor.anjul124@gmail.com>
2026-01-16 13:24:19 +0530

    Update admin whitelist to specific emails

--- src/backend/controllers/userController.js
+++ src/backend/controllers/userController.js
@@ -12,9 +12,8 @@ const generateToken = (id) => {
 
 // Hardcoded Admin Whitelist
 const ADMIN_EMAILS = [
-    "admin@apthire.com",
-    "ceo@apthire.com",
-    "anjulrathor.dev@gmail.com"
+    "anjulrathor.dev@gmail.com",
+    "apthire.care@gmail.com"
 ];
 
 // @desc    Register new user & Send OTP
